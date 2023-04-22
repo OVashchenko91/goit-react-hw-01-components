a951ee54c6c57d52a16b7fcba8ea048a49b488dd
Oksana <oksana.sydorenko0691@icloud.com>
2023-04-22 09:24:47 +0300

    add

---
+++ src/components/Friends/FriendList.js
@@ -0,0 +1,34 @@
+
+ 
+
+import PropTypes from 'prop-types';
+import css from './FriendList.module.css';
+import { FriendListItem } from '../FriendListItem/FriendListItem';
+
+export function FriendList({friends}) {
+    return (
+        <ul className={css.friends}>
+            {friends.map(({
+                id, avatar, name, isOnline
+            }) => {
+                return (
+                    <FriendListItem 
+                        key = {id}
+                        name = {name}
+                        avatar = {avatar}
+                        isOnline = {isOnline}
+                    />                 
+                );
+            })}
+        </ul>
+    );
+};
+
+FriendList.propTypes = {
+    friends: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.oneOfType([
+            PropTypes.string.isRequired,
+            PropTypes.number.isRequired,
+        ]),
+    })),
+};
